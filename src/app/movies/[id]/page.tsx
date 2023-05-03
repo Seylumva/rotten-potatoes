a37973ee46736cd26f1f9a83984a3a1db4606f82
SeylumVA <sc2nem@gmail.com>
2023-05-03 03:51:00 -0400

    Fixed details page API response type

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -16,13 +16,13 @@ async function getDetails(id) {
 export async function generateMetadata({ params }) {
   const data = await getDetails(params.id);
   return {
-    title: `Rotten Potatoes | ${data.movie.title}`,
+    title: `Rotten Potatoes | ${data.title}`,
   };
 }
 
 export default async function MovieDetails({ params }) {
   const data = await getDetails(params.id);
-  const genres = data.movie.genres
+  const genres = data.genres
     .map((genre) => {
       return genre.name;
     })
@@ -32,37 +32,31 @@ export default async function MovieDetails({ params }) {
       <div className="flex gap-5 flex-col md:flex-row">
         <Image
           className="rounded-lg shadow-md shadow-black self-center md:self-stretch"
-          src={`https://image.tmdb.org/t/p/original/${data.movie.poster_path}`}
-          alt={`${data.movie.title} Poster`}
+          src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
+          alt={`${data.title} Poster`}
           width={250}
           height={375}
         />
         <div className="flex flex-col justify-between flex-grow rounded-lg bg-gray-200 px-4 py-2">
           {/* Basic Info */}
           <div>
-            <h1 className="text-2xl text-center font-bold">
-              {data.movie.title}
-            </h1>
+            <h1 className="text-2xl text-center font-bold">{data.title}</h1>
             <p className="text-sm text-center font-light">{genres}</p>
           </div>
           {/* Scores */}
           <div className="flex justify-evenly mt-5">
             <div className="flex flex-col gap-1 text-center">
-              <span className="text-2xl font-black">
-                {data.movie.vote_average}
-              </span>
+              <span className="text-2xl font-black">{data.vote_average}</span>
               <span className="text-sm uppercase font-black">Potatometer</span>
             </div>
             <div className="flex flex-col gap-1 text-center">
-              <span className="text-2xl font-black">
-                {data.movie.vote_count}
-              </span>
+              <span className="text-2xl font-black">{data.vote_count}</span>
               <span className="text-sm uppercase font-black">Votes</span>
             </div>
           </div>
           {/* Studios */}
           <div className="flex justify-evenly mt-4">
-            {data.movie.production_companies
+            {data.production_companies
               .filter((studio) => studio.logo_path !== null)
               .map((studio) => (
                 <div
